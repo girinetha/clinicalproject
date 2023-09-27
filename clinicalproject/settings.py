8e637330c18b26d6b834f0152b3b17a3dbff8f15
girinetha <144089509+girinetha@users.noreply.github.com>
2023-09-27 12:38:32 +0530

    Update settings.py

--- clinicalproject/settings.py
+++ clinicalproject/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'clinicalproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': 'clinicaldb',
         'HOST':'localhost',
         'PASSWORD':'9696',
